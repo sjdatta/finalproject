84f2c9d1189e9bd06fbe4c77c6b600723b9f7dbb
sjdatta <sarah.datta@gmail.com>
2024-05-03 11:39:20 -0400

    average and example file

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 fn main() {
-    #final project code
+    
 }
 
 
@@ -18,5 +18,47 @@ fn read_file(path: &str) -> Vec<(usize, usize)> {
 }
 
 
+//organizing the output from read_file into an easier to manipulate vector
+fn organize(edges: Vec<(usize, usize)>) -> Vec<Vec<usize>> {
+    let n = findmax(edges.clone()) + 1;
+    let mut graph_list : Vec<Vec<usize>> = vec![vec![];n];
+    for (v,w) in edges.iter() {
+        graph_list[*v].push(*w);
+        graph_list[*w].push(*v);
+    
+    };
+    graph_list
+}
 
-#code code code
+
+//finding the max vertex to know how long I need to make the vector
+fn findmax(edges: Vec<(usize, usize)>) -> usize {
+    let mut max = edges[0].1;
+    for i in 0..edges.len() {
+        if max < edges[i].0 {
+            max = edges[i].0;
+        } else if max < edges[i].1 {
+            max = edges[i].1;
+        }
+    }
+
+    max
+}
+
+
+//let's find some out some basic info about our dataset
+
+
+//finding average number of products copurchased
+
+fn findaverage(v: Vec<Vec<usize>>) -> Vec<usize> {
+    let n = findmax(v); 
+    let mut tally = vec![0; v.len()]
+    for i in 0..v.len() {
+        let m = v[i];
+        tally[m[0]] += 1;
+
+    }
+
+    tally
+}
